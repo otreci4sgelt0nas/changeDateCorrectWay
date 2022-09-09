c4d95aba4fdab9c507d873a0d4e3c5ab69993a9a
otrecias geltonas <otreci4sgelt0nas@gmail.com>
2022-09-09 12:23:21 +0300

    yolo

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import re
 
 def changeDateCorrectWay(txt):
-    d = re.search("[0-2][0-9]:[0-5][0-9] [0-3][0-9]\/[0-1][0-9]\/\d{4}", txt)
-    if d == None:
-        print('z')
+    d1 = re.search("[0-2][0-9]:[0-5][0-9]",txt[:4])
+    d2 = re.search("[0-3][0-9]\/[0-1][0-9]\/\d{4}",txt[5:])
+    # print(d1)
+    if d1 == None:
+        print(txt[11:]+' '+txt[::-1])
+
     # d = d.group()
-    print(d)
-    return d
+    print(txt)
 
-date1 = '18:11 23/08/2022'
+# date1 = '18:11 23/08/2022'
 
-changeDateCorrectWay(date1)
+# changeDateCorrectWay(date1)
 
 date2 = '2022/08/18 00:30'
+
+
 
 changeDateCorrectWay(date2)
